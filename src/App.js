3f3159fa242d57670d396dac2619ff814fbcb9cd
kbaginski <106392277+kAArter33@users.noreply.github.com>
2022-07-25 18:18:33 +0200

    crypto-rate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Crypto from './Crypto';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Crypto Rate</h1>
+      </header>
+      <Crypto />
+    </div>
+  );
+}
+
+export default App;
